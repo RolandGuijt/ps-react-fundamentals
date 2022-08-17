07dc278860db299936e274551b5cc770a0a36298
Roland Guijt <roland.guijt@gmail.com>
2022-08-17 13:24:04 +0200

    Add useGetRequest hook

--- hooks/useHouses.js
+++ hooks/useHouses.js
@@ -1,24 +1,18 @@
 import { useEffect, useState } from "react";
 import loadingStatus from "../helpers/loadingStatus";
+import useGetRequest from "./useGetRequest";
 
 const useHouses = () => {
   const [houses, setHouses] = useState([]);
-  const [loadingState, setLoadingState] = useState(loadingStatus.isLoading);
+  const { get, loadingState } = useGetRequest("/api/houses");
 
   useEffect(() => {
     const fetchHouses = async () => {
-      setLoadingState(loadingStatus.isLoading);
-      try {
-        const response = await fetch("/api/houses");
-        const houses = await response.json();
-        setHouses(houses);
-        setLoadingState(loadingStatus.loaded);
-      } catch {
-        setLoadingState(loadingStatus.hasErrored);
-      }
+      const houses = await get();
+      setHouses(houses);
     };
     fetchHouses();
-  }, []);
+  }, [get]);
 
   return { houses, setHouses, loadingState };
 };
